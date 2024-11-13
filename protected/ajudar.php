eac02b127f0362835a27939fdfea25561ff82048
Raul Lize Teixeira <114193996+Raullize@users.noreply.github.com>
2024-11-13 09:41:28 -0300

    fix: arrumar

--- protected/ajudar.php
+++ protected/ajudar.php
@@ -2,7 +2,8 @@
 
 session_start();
 
-if (!isset($_SESSION['usuario'])) {
+
+if (!isset($_SESSION['usuario']) && !isset($_SESSION['empresa'])) {
     header("Location: ../public/login.php");
     exit;
 }
